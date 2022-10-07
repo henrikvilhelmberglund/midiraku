effe0ae9a05b43de0a6132ab38b73358ab9481b4
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-07 16:54:21 +0200

    add CSS

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
-let noteDivs = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+let noteDivs = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"].reverse();
+let mainDiv = document.createElement("div");
+document.body.append(mainDiv);
+
 
 function addDivs() {
   noteDivs.forEach((div) => {
     let noteDiv = document.createElement("div");
     noteDiv.id = div;
-    document.body.append(noteDiv);
+    noteDiv.className = "note-div";
+    noteDiv.addEventListener("click", (e) => addNote(e));
+    mainDiv.append(noteDiv);
   });
 }
 
 addDivs();
 
-function addNote(params) {
+function addNote(e) {
   let newNote = document.createElement("button");
-  newNote.innerText("Note");
+  newNote.innerText = "Note";
   newNote.addEventListener("click", (e) => moveNote(e));
+  if (e.target.className === "note-div") {
+    e.target.append(newNote);
+  }
+
 }
 
