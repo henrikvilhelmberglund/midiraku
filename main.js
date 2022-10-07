1ea6c6083a9bc8e9d94d7b3ff7c78987602f1ce5
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-07 17:16:21 +0200

    add horizontal divisions

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
 let noteDivs = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"].reverse();
 let mainDiv = document.createElement("div");
+mainDiv.id = "main-div";
 document.body.append(mainDiv);
 
+let horizontalDivision = 8;
 
-function addDivs() {
+
+
+function addVerticalDivs() {
   noteDivs.forEach((div) => {
     let noteDiv = document.createElement("div");
     noteDiv.id = div;
-    noteDiv.className = "note-div";
-    noteDiv.addEventListener("click", (e) => addNote(e));
+
+    noteDiv.className = "vertical-note-div";
+    noteDiv.addEventListener("mousedown", (e) => addNote(e));
     mainDiv.append(noteDiv);
+    for (let i = 0; i < horizontalDivision; i++) {
+      let horizontalDiv = document.createElement("div");
+      horizontalDiv.className = "horizontal-note-div";
+      noteDiv.append(horizontalDiv);
+    }
   });
 }
 
-addDivs();
+addVerticalDivs();
 
 function addNote(e) {
   let newNote = document.createElement("button");
   newNote.innerText = "Note";
   newNote.addEventListener("click", (e) => moveNote(e));
-  if (e.target.className === "note-div") {
-    e.target.append(newNote);
+  if (e.target.className === "vertical-note-div" || e.target.className === "horizontal-note-div") {
+    if (e.target.children.length < 1) {
+      e.target.append(newNote);
+    }
   }
 
 }
