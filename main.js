072a99923b140b41ea619db6894462cfe13af86d
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-07 19:17:31 +0200

    add WEBMIDI.js and wrap everything in main()

--- main.js
+++ main.js
@@ -1,137 +1,70 @@
-// from https://webmidi-examples.glitch.me
+let midiOut;
+// from https://webmidijs.org/docs/getting-started/basics
+// Enable WEBMIDI.js and trigger the onEnabled() function when ready
 
-let midiIn = [];
-let midiOut = [];
-let selectedIndex = 0;
+WebMidi
+  .enable()
+  .then(onEnabled)
+  .then(main)
+  .catch(err => alert(err));
 
-function connect() {
-  navigator.requestMIDIAccess()
-    .then(
-      (midi) => midiReady(midi),
-      (err) => console.log('Something went wrong', err));
-}
+// Function triggered when WEBMIDI.js is ready
+function onEnabled() {
 
-function midiReady(midi) {
-  // Also react to device changes.
-  midi.addEventListener('statechange', (event) => initDevices(event.target));
-  initDevices(midi); // see the next section!
-}
-
-function initDevices(midi) {
-  // Reset.
-  midiIn = [];
-  midiOut = [];
-
-  // MIDI devices that send you data.
-  const inputs = midi.inputs.values();
-  for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
-    midiIn.push(input.value);
-  }
-
-  // MIDI devices that you send data to.
-  const outputs = midi.outputs.values();
-  for (let output = outputs.next(); output && !output.done; output = outputs.next()) {
-    midiOut.push(output.value);
-  }
-
-  displayDevices();
-  startListening();
-}
-
-
-// Start listening to MIDI messages.
-function startListening() {
-  for (const input of midiIn) {
-    input.addEventListener('midimessage', midiMessageReceived);
+  // Display available MIDI input devices
+  if (WebMidi.outputs.length < 1) {
+    document.body.innerHTML += "No device detected.";
+  } else {
+    WebMidi.outputs.forEach((device, index) => {
+      document.body.innerHTML += `${index}: ${device.name} <br>`;
+      midiOut = WebMidi.outputs[index];
+      console.log(midiOut);
+    });
   }
 }
 
-function midiMessageReceived(event) {
-  // MIDI commands we care about. See
-  // http://webaudio.github.io/web-midi-api/#a-simple-monophonic-sine-wave-midi-synthesizer.
-  const NOTE_ON = 9;
-  const NOTE_OFF = 8;
+function main() {
 
-  const cmd = event.data[0] >> 4;
-  const pitch = event.data[1];
-  const velocity = (event.data.length > 2) ? event.data[2] : 1;
+  console.log(midiOut);
 
-  // You can use the timestamp to figure out the duration of each note.
-  const timestamp = Date.now();
+  let noteDivs = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"].reverse();
+  let mainDiv = document.createElement("div");
+  mainDiv.id = "main-div";
+  document.body.append(mainDiv);
 
-  // Note that not all MIDI controllers send a separate NOTE_OFF command for every NOTE_ON.
-  if (cmd === NOTE_OFF || (cmd === NOTE_ON && velocity === 0)) {
-    console.log(`🎧 from ${event.srcElement.name} note off: pitch:${pitch}, velocity: ${velocity}`);
+  let horizontalDivision = 8;
 
-    // Complete the note!
-    const note = notesOn.get(pitch);
-    if (note) {
-      console.log(`🎵 pitch:${pitch}, duration:${timestamp - note} ms.`);
-      notesOn.delete(pitch);
-    }
-  } else if (cmd === NOTE_ON) {
-    console.log(`🎧 from ${event.srcElement.name} note off: pitch:${pitch}, velocity: {velocity}`);
+  function addVerticalDivs() {
+    noteDivs.forEach((div) => {
+      let noteDiv = document.createElement("div");
+      noteDiv.id = div;
 
-    // One note can only be on at once.
-    notesOn.set(pitch, timestamp);
+      noteDiv.className = "vertical-note-div";
+      noteDiv.addEventListener("mousedown", (e) => addNote(e));
+      mainDiv.append(noteDiv);
+      for (let i = 0; i < horizontalDivision; i++) {
+        let horizontalDiv = document.createElement("div");
+        horizontalDiv.className = "horizontal-note-div";
+        noteDiv.append(horizontalDiv);
+      }
+    });
   }
-}
 
-function sendMidiMessage(pitch, velocity, duration) {
-  const NOTE_ON = 0x90;
-  const NOTE_OFF = 0x80;
+  addVerticalDivs();
 
-  // const device = midiOut[selectOut.selectedIndex];
-  const device = midiOut[selectedIndex];
-  const msgOn = [NOTE_ON, pitch, velocity];
-  const msgOff = [NOTE_ON, pitch, velocity];
-
-  // First send the note on;
-  device.send(msgOn);
-
-  // Then send the note off. You can send this separately if you want 
-  // (i.e. when the button is released)
-  device.send(msgOff, Date.now() + duration);
-}
-
-let noteDivs = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"].reverse();
-let mainDiv = document.createElement("div");
-mainDiv.id = "main-div";
-document.body.append(mainDiv);
-
-let horizontalDivision = 8;
-
-
-
-function addVerticalDivs() {
-  noteDivs.forEach((div) => {
-    let noteDiv = document.createElement("div");
-    noteDiv.id = div;
-
-    noteDiv.className = "vertical-note-div";
-    noteDiv.addEventListener("mousedown", (e) => addNote(e));
-    mainDiv.append(noteDiv);
-    for (let i = 0; i < horizontalDivision; i++) {
-      let horizontalDiv = document.createElement("div");
-      horizontalDiv.className = "horizontal-note-div";
-      noteDiv.append(horizontalDiv);
-    }
-  });
-}
-
-addVerticalDivs();
-
-function addNote(e) {
-  let newNote = document.createElement("button");
-  newNote.innerText = "Note";
-  newNote.addEventListener("click", (e) => moveNote(e));
-  if (e.target.className === "vertical-note-div" || e.target.className === "horizontal-note-div") {
-    if (e.target.children.length < 1) {
-      e.target.append(newNote);
+  function addNote(e) {
+    let newNote = document.createElement("button");
+    newNote.innerText = "Note";
+    newNote.addEventListener("click", (e) => moveNote(e));
+    if (e.target.className === "vertical-note-div" || e.target.className === "horizontal-note-div") {
+      if (e.target.children.length < 1) {
+        e.target.append(newNote);
+      }
     }
   }
-}
-// test
-connect();
-sendMidiMessage(61, 100, 100);
+  // test
+  //connect();
+  //sendMidiMessage(61, 100, 100);
 
+
+}
