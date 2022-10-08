eca52985be7d44fd88ae067aa6ca5351331e22b5
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-08 17:09:28 +0200

    update getting DOMException DOMException: An attempt was made to break through the security policy of the user agent. in Edge Devtools in VSCode

--- main.js
+++ main.js
@@ -1,70 +1,65 @@
-let midiOut;
 // from https://webmidijs.org/docs/getting-started/basics
 // Enable WEBMIDI.js and trigger the onEnabled() function when ready
 
-WebMidi
-  .enable()
-  .then(onEnabled)
-  .then(main)
-  .catch(err => alert(err));
-
-// Function triggered when WEBMIDI.js is ready
-function onEnabled() {
-
-  // Display available MIDI input devices
-  if (WebMidi.outputs.length < 1) {
-    document.body.innerHTML += "No device detected.";
-  } else {
-    WebMidi.outputs.forEach((device, index) => {
-      document.body.innerHTML += `${index}: ${device.name} <br>`;
-      midiOut = WebMidi.outputs[index];
-      console.log(midiOut);
-    });
-  }
-}
-
-function main() {
-
-  console.log(midiOut);
-
-  let noteDivs = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"].reverse();
-  let mainDiv = document.createElement("div");
-  mainDiv.id = "main-div";
-  document.body.append(mainDiv);
-
-  let horizontalDivision = 8;
-
-  function addVerticalDivs() {
-    noteDivs.forEach((div) => {
-      let noteDiv = document.createElement("div");
-      noteDiv.id = div;
-
-      noteDiv.className = "vertical-note-div";
-      noteDiv.addEventListener("mousedown", (e) => addNote(e));
-      mainDiv.append(noteDiv);
-      for (let i = 0; i < horizontalDivision; i++) {
-        let horizontalDiv = document.createElement("div");
-        horizontalDiv.className = "horizontal-note-div";
-        noteDiv.append(horizontalDiv);
-      }
-    });
-  }
-
-  addVerticalDivs();
-
-  function addNote(e) {
-    let newNote = document.createElement("button");
-    newNote.innerText = "Note";
-    newNote.addEventListener("click", (e) => moveNote(e));
-    if (e.target.className === "vertical-note-div" || e.target.className === "horizontal-note-div") {
-      if (e.target.children.length < 1) {
-        e.target.append(newNote);
-      }
+const myMidi = await WebMidi.enable();
+//let midiOut = myMidi.outputs[0];
+//console.log(midiOut);
+/*
+  // Function triggered when WEBMIDI.js is ready
+  function onEnabled() {
+  
+    // Display available MIDI input devices
+    if (WebMidi.outputs.length < 1) {
+      document.body.innerHTML += "No device detected.";
+    } else {
+      WebMidi.outputs.forEach((device, index) => {
+        document.body.innerHTML += `${index}: ${device.name} <br>`;
+        midiOut = WebMidi.outputs[index];
+        console.log(midiOut);
+      });
     }
   }
+*/
+
+//console.log(midiOut);
+
+let noteDivs = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"].reverse();
+let mainDiv = document.createElement("div");
+mainDiv.id = "main-div";
+document.body.append(mainDiv);
+
+let horizontalDivision = 8;
+
+function addVerticalDivs() {
+  noteDivs.forEach((div) => {
+    let noteDiv = document.createElement("div");
+    noteDiv.id = div;
+
+    noteDiv.className = "vertical-note-div";
+    noteDiv.addEventListener("mousedown", (e) => addNote(e));
+    mainDiv.append(noteDiv);
+    for (let i = 0; i < horizontalDivision; i++) {
+      let horizontalDiv = document.createElement("div");
+      horizontalDiv.className = "horizontal-note-div";
+      noteDiv.append(horizontalDiv);
+    }
+  });
+}
+
+addVerticalDivs();
+
+function addNote(e) {
+  let newNote = document.createElement("button");
+  newNote.innerText = "Note";
+  newNote.addEventListener("click", (e) => moveNote(e));
+  if (e.target.className === "vertical-note-div" || e.target.className === "horizontal-note-div") {
+    if (e.target.children.length < 1) {
+      e.target.append(newNote);
+    }
+  }
+}
   // test
   //connect();
   //sendMidiMessage(61, 100, 100);
 
 
-}
