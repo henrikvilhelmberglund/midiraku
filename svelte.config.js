fba0dc0a6e5a2b63d67e30b160333102cb156411
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-16 18:06:33 +0100

    fix: :bug: add paths to svelte.config.js
    I always forget this too

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@
 import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
+const dev = process.argv.includes("dev");
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -13,7 +15,10 @@ const config = {
 			fallback: null,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: dev ? "" : "/midiraku"
+		}
 	},
 	preprocess: [
 		preprocess({
