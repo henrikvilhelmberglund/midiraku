87341f43f4b256b1174360a0eb11ea9485b802e4
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-09 11:35:05 +0100

    fix: :bug: change adapter for vercel support

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 const dev = process.argv.includes("dev");
@@ -7,15 +7,7 @@ const dev = process.argv.includes("dev");
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: "build",
-			assets: "build",
-			fallback: null,
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(),
 		paths: {
 			base: dev ? "" : "/midiraku"
 		}
