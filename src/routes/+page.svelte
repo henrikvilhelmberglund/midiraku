196cd54d63e3538029c78ac53c36bc44a567a83c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-09 11:27:36 +0100

    feat: :sparkles: add octave shift, add jzz-synth-tiny

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
 <script>
 	import JZZ from "jzz";
+	import TINY from "jzz-synth-tiny";
+	TINY(JZZ);
 	import { RangeSlider } from "@skeletonlabs/skeleton";
 	import { instruments } from "./instruments.js";
+	// JZZ.synth.Tiny.register("Web Audio");
 	let midi = JZZ().openMidiOut();
+	var synth = JZZ.synth.Tiny();
+
 	// midi;
 
 	// midi
@@ -10,73 +15,74 @@
 	// 	.send([0x90, 61, 127]) // note on
 	// 	.wait(500)
 	// 	.send([0x80, 61, 0]); // note off
-	let notes = [
-		"C4",
-		"C#4",
-		"D4",
-		"D#4",
-		"E4",
-		"F4",
-		"F#4",
-		"G4",
-		"G#4",
-		"A4",
-		"A#4",
-		"B4",
-		"C5",
-		"C#5",
-		"D5",
-		"D#5",
-		"E5",
-		"F5",
-		"F#5",
-		"G5",
-		"G#5",
-		"A5",
-		"A#5",
-		"B5",
-		"C6",
-		"C#6",
-		"D6",
-		"D#6",
-		"E6"
+	let octaveShift = 0;
+	$: notes = [
+		`C${4 + octaveShift}`,
+		`C#${4 + octaveShift}`,
+		`D${4 + octaveShift}`,
+		`D#${4 + octaveShift}`,
+		`E${4 + octaveShift}`,
+		`F${4 + octaveShift}`,
+		`F#${4 + octaveShift}`,
+		`G${4 + octaveShift}`,
+		`G#${4 + octaveShift}`,
+		`A${4 + octaveShift}`,
+		`A#${4 + octaveShift}`,
+		`B${4 + octaveShift}`,
+		`C${5 + octaveShift}`,
+		`C#${5 + octaveShift}`,
+		`D${5 + octaveShift}`,
+		`D#${5 + octaveShift}`,
+		`E${5 + octaveShift}`,
+		`F${5 + octaveShift}`,
+		`F#${5 + octaveShift}`,
+		`G${5 + octaveShift}`,
+		`G#${5 + octaveShift}`,
+		`A${5 + octaveShift}`,
+		`A#${5 + octaveShift}`,
+		`B${5 + octaveShift}`,
+		`C${6 + octaveShift}`,
+		`C#${6 + octaveShift}`,
+		`D${6 + octaveShift}`,
+		`D#${6 + octaveShift}`,
+		`E${6 + octaveShift}`
 	];
-	let noteKeys = [
-		{ key: "KeyZ", note: "C4" },
-		{ key: "KeyS", note: "C#4" },
-		{ key: "KeyX", note: "D4" },
-		{ key: "KeyD", note: "D#4" },
-		{ key: "KeyC", note: "E4" },
-		{ key: "KeyV", note: "F4" },
-		{ key: "KeyG", note: "F#4" },
-		{ key: "KeyB", note: "G4" },
-		{ key: "KeyH", note: "G#4" },
-		{ key: "KeyN", note: "A4" },
-		{ key: "KeyJ", note: "A#4" },
-		{ key: "KeyM", note: "B4" },
-		{ key: "Comma", note: "C5" },
-		{ key: "KeyL", note: "C#5" },
-		{ key: "Period", note: "D5" },
-		{ key: "Semicolon", note: "D#5" },
-		{ key: "Slash", note: "E5" },
-		{ key: "KeyQ", note: "B4" },
-		{ key: "KeyW", note: "C5" },
-		{ key: "Digit3", note: "C#5" },
-		{ key: "KeyE", note: "D5" },
-		{ key: "Digit4", note: "D#5" },
-		{ key: "KeyR", note: "E5" },
-		{ key: "KeyT", note: "F5" },
-		{ key: "Digit6", note: "F#5" },
-		{ key: "KeyY", note: "G5" },
-		{ key: "Digit7", note: "G#5" },
-		{ key: "KeyU", note: "A5" },
-		{ key: "Digit8", note: "A#5" },
-		{ key: "KeyI", note: "B5" },
-		{ key: "KeyO", note: "C6" },
-		{ key: "Digit0", note: "C#6" },
-		{ key: "KeyP", note: "D6" },
-		{ key: "Minus", note: "D#6" },
-		{ key: "BracketLeft", note: "E6" }
+	$: noteKeys = [
+		{ key: "KeyZ", note: `C${4 + octaveShift}` },
+		{ key: "KeyS", note: `C#${4 + octaveShift}` },
+		{ key: "KeyX", note: `D${4 + octaveShift}` },
+		{ key: "KeyD", note: `D#${4 + octaveShift}` },
+		{ key: "KeyC", note: `E${4 + octaveShift}` },
+		{ key: "KeyV", note: `F${4 + octaveShift}` },
+		{ key: "KeyG", note: `F#${4 + octaveShift}` },
+		{ key: "KeyB", note: `G${4 + octaveShift}` },
+		{ key: "KeyH", note: `G#${4 + octaveShift}` },
+		{ key: "KeyN", note: `A${4 + octaveShift}` },
+		{ key: "KeyJ", note: `A#${4 + octaveShift}` },
+		{ key: "KeyM", note: `B${4 + octaveShift}` },
+		{ key: "Comma", note: `C${5 + octaveShift}` },
+		{ key: "KeyL", note: `C#${5 + octaveShift}` },
+		{ key: "Period", note: `D${5 + octaveShift}` },
+		{ key: "Semicolon", note: `D#${5 + octaveShift}` },
+		{ key: "Slash", note: `E${5 + octaveShift}` },
+		{ key: "KeyQ", note: `B${4 + octaveShift}` },
+		{ key: "KeyW", note: `C${5 + octaveShift}` },
+		{ key: "Digit3", note: `C#${5 + octaveShift}` },
+		{ key: "KeyE", note: `D${5 + octaveShift}` },
+		{ key: "Digit4", note: `D#${5 + octaveShift}` },
+		{ key: "KeyR", note: `E${5 + octaveShift}` },
+		{ key: "KeyT", note: `F${5 + octaveShift}` },
+		{ key: "Digit6", note: `F#${5 + octaveShift}` },
+		{ key: "KeyY", note: `G${5 + octaveShift}` },
+		{ key: "Digit7", note: `G#${5 + octaveShift}` },
+		{ key: "KeyU", note: `A${5 + octaveShift}` },
+		{ key: "Digit8", note: `A#${5 + octaveShift}` },
+		{ key: "KeyI", note: `B${5 + octaveShift}` },
+		{ key: "KeyO", note: `C${6 + octaveShift}` },
+		{ key: "Digit0", note: `C#${6 + octaveShift}` },
+		{ key: "KeyP", note: `D${6 + octaveShift}` },
+		{ key: "Minus", note: `D#${6 + octaveShift}` },
+		{ key: "BracketLeft", note: `E${6 + octaveShift}` }
 		// "Digit6",
 		// "KeyY",
 		// "Digit7",
@@ -95,18 +101,16 @@
 	let currentProgram = 0;
 	let mouseDown;
 
-	// function changeProgram(program) {
-	// 	midi.program = program;
-	// 	console.log(program);
-	// }
 
 	$: {
 		midi.program(0, currentProgram);
+		synth.program(0, currentProgram);
 	}
 
 	function playNote(note) {
 		if (mouseDown) {
 			midi.send([0x90, note, 127]); // note off
+			synth.send([0x90, note, 127]);
 			pressedKey = document.querySelector("#" + note.replace("#", "-"));
 			if (note.includes("#")) {
 				pressedKey.classList.add("pressed2");
@@ -118,6 +122,8 @@
 
 	function releaseNote(note) {
 		midi.send([0x80, note, 0]); // note off
+		synth.send([0x80, note, 0]); // note off
+
 		pressedKey = document.querySelector("#" + note.replace("#", "-"));
 		if (note.includes("#")) {
 			pressedKey.classList.remove("pressed2");
@@ -133,9 +139,8 @@
 		if (pressedKey.classList.contains("pressed")) {
 			return;
 		} else {
-			// const pos = myArray.map((e) => e.hello).indexOf("stevie");
-			// console.log(finalNote);
 			midi.send([0x90, finalNote, 127]); // note off
+			synth.send([0x90, finalNote, 127]); // note off
 			pressedKey.classList.add("pressed");
 		}
 	}
@@ -144,26 +149,47 @@
 		// console.log(e);
 	}
 	function keyUp(code) {
-		// console.log(code);
 		let finalNoteIndex = noteKeys.findIndex((i) => i.key === code);
 		let finalNote = noteKeys[finalNoteIndex].note;
 		midi.send([0x80, finalNote, 0]);
+		synth.send([0x80, finalNote, 0]);
 		releasedKey = document.querySelector("#" + finalNote.replace("#", "-"));
 		releasedKey.classList.remove("pressed");
 	}
+
+	function shiftOctave(direction) {
+		if (direction === 1) {
+			octaveShift += 1;
+		} else {
+			octaveShift -= 1;
+		}
+	}
 </script>
 
-<main class="flex h-full flex-col items-center justify-center">
-  <div>
-  
-    {#await midi then value}
+<div class="absolute">
+  {#await midi then value}
     <!-- {console.log(value.info())} -->
     {#each Object.entries(value.info()) as info}
-    <p>{info},</p>
+      <p>{info},</p>
     {/each}
-    {/await}
+  {/await}
+</div>
+<main class="flex h-full flex-col items-center justify-center">
+  <div class="flex flex-col items-center">
+    <p>Octave shift</p>
+    <div class="flex">
+      <button
+			class="rounded-lg bg-slate-100 p-1 px-4 text-4xl text-black"
+			on:click={() => shiftOctave(-1)}>-</button>
+      <p class="p-4 !text-4xl">
+        {octaveShift}
+      </p>
+      <button
+			class="rounded-lg bg-slate-100 p-1 px-4 text-4xl text-black"
+			on:click={() => shiftOctave(1)}>+</button>
+    </div>
   </div>
-	<RangeSlider bind:value={currentProgram} max={127} step={1}  class="p-12 w-3/4"
+	<RangeSlider bind:value={currentProgram} max={127} step={1} class="w-3/4 p-12"
 		>{instruments[currentProgram]}</RangeSlider>
 	<div>
 		<article
