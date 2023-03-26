9ed19dea3fb32524ef9adb41873b15076a60246d
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-26 22:36:32 +0200

    feat: :sparkles: add stuff in N component

--- src/lib/N.svelte
+++ src/lib/N.svelte
@@ -1,8 +1,48 @@
 <script>
+	import { Note, sortedNotes, noteList } from "./note";
+	export let songNote;
 	export let noteValue;
+
+	let moving = 0;
+	let startX;
+	let startY;
+
+	let m = { x: 0, y: 0 };
+
+	function handleMousemove(event) {
+		m.x = event.clientX;
+		m.y = event.clientY;
+	}
+
+	function setInitialValues(event) {
+		m.x = event.clientX;
+		m.y = event.clientY;
+		startX = m.x;
+		startY = m.y;
+	}
+
+	function moveNote() {
+		console.log("what");
+
+		console.table(`startY: ${startY} y: ${m.y}`);
+
+		// TODO fix (songNote probably doesn't update)
+		if (startY - m.y < -20) {
+			songNote.noteValue = noteList[noteList.indexOf(songNote.noteValue) - 1];
+		}
+	}
 </script>
 
-<button class="absolute bottom-0 left-0 rounded bg-blue-400 p-1">{noteValue}</button>
+<button
+	draggable="true"
+	on:mousedown|stopPropagation={() => (moving = 1)}
+	on:mouseup={() => (moving = 0)}
+	on:dragstart={(e) => setInitialValues(e)}
+	on:drag={(e) => {
+		handleMousemove(e);
+		moveNote();
+	}}
+	class="absolute bottom-0 left-0 rounded bg-blue-400 p-1">{noteValue}</button>
 
 <style>
 </style>
